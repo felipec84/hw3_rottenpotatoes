04b117d94ed33402d2fe4baf04dc528076766e09
Felipe Concha Avello (casa) <felipec84@gmail.com>
2013-08-04 19:35:47 -0400

    Verifica el orden para sort
    Para esto utilizar expersiones regulares, con este commit ya se llegan
    a los 500 pts solicitados en la HW3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert_match(/<td>#{e1}<\/td>.*<td>#{e2}<\/td>/m,page.body)
+  #puts page.body
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once

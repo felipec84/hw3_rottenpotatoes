80cb3cfb36be6d1811f1afffe3d44258bf6706ba
Felipe Concha Avello (casa) <felipec84@gmail.com>
2013-08-12 22:53:20 -0400

    Primeras definicione de rspec
    Solo define las clausulas con it, notar que se ejecuta con:
    * bundle exec autotest

---
+++ spec/movies_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'find movie with same director' do
+    it 'should call the model method that performs the search'
+    it 'should select the Director Results template for rendering'
+    it 'should make the Director Resutls available to that template'
+  end
+end

6bbea21b1c4ccad0d5da54e2758794e5c2285087
Felipe Concha Avello (casa) <felipec84@gmail.com>
2013-08-12 23:12:06 -0400

    Crea un post y crea la ruta

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe MoviesController do
   describe 'find movie with same director' do
     it 'should call the model method that performs the search' do
+      Movie.should_receive(:find_by_same_director).with(1)
+        post :same_director, {:id => 1}
+      end
     it 'should select the Director Results template for rendering'
     it 'should make the Director Resutls available to that template'
   end

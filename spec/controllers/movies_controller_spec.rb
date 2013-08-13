05ddc8b7962e92223bd15c2f3a75dee47c0d1806
Felipe Concha Avello (casa) <felipec84@gmail.com>
2013-08-12 23:06:01 -0400

    Mueve a ubicacion correcta spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'find movie with same director' do
-    it 'should call the model method that performs the search'
+    it 'should call the model method that performs the search' do
     it 'should select the Director Results template for rendering'
     it 'should make the Director Resutls available to that template'
   end
